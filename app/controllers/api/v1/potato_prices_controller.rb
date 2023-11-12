50b811396cca169bad9c699261ad444aadfc8d69
totycos <dv94@hotmail.fr>
2023-11-12 19:41:54 +0100

    feat(PotatoPrices): Set methods in controller - daily_prices and max_potential_gain methods

--- app/controllers/api/v1/potato_prices_controller.rb
+++ app/controllers/api/v1/potato_prices_controller.rb
@@ -1,2 +1,33 @@
 class Api::V1::PotatoPricesController < ApplicationController
+
+  def daily_prices
+    potato_prices = get_potato_prices_by_date(params[:date])
+    render json: potato_prices
+  end
+
+  def max_potential_gain
+    potato_prices = get_potato_prices_by_date(params[:date])
+    buy_price = potato_prices.first[:price]
+    gain = 0
+
+    potato_prices.each do |data|
+        current_price = data[:price]
+        if current_price < buy_price
+            buy_price = current_price
+        else
+            current_gain = current_price - buy_price
+            gain = [gain, current_gain].max
+        end
+    end
+
+    render json: { max_gain: "#{gain}€" }
+    
+  end
+  
+private 
+
+  def get_potato_prices_by_date(date)
+    PotatoPrice.where(time: date.to_datetime.beginning_of_day..date.to_datetime.end_of_day).order(:time)
+  end
+
 end
