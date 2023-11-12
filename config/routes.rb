5a28090c7c659b5e45cabfe6fcf29c65d14aa798
totycos <dv94@hotmail.fr>
2023-11-12 18:29:49 +0100

    build: Set routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  
+  namespace :api do
+    namespace :v1 do
+      get 'potato_prices/:date/daily_prices', to: 'potato_prices#daily_prices'
+      get 'potato_prices/:date/max_potential_gain', to: 'potato_prices#max_potential_gain'
+    end
+  end
+  
 end
